b2008b01ff31a71d4abb0ab4b026760dd7bef293
9873004 <66959167+9873004@users.noreply.github.com>
2020-06-22 19:10:31 +0530

    Add files via upload

---
+++ Wipro PRP/src/Program5.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n%2==0) {
+			System.out.println("Even NUmber");
+		}
+		else {
+			System.out.println("Odd Number");
+		}
+
+	}
+
+}
